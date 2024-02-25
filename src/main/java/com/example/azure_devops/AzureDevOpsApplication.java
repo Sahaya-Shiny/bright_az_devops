898daa2dc81ac49236e5b0b3a0cdfb0d2f3c305b
Sahaya-Shiny <shinyasir@gmail.com>
2024-02-25 13:14:47 +0530

    Initial commit

---
+++ src/main/java/com/example/azure_devops/AzureDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDevOpsApplication.class, args);
+	}
+
+}
